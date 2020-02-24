28da80e08850f301f42841e7c8fc09cd48dcfd7f
Kyle Peacock <kyle.peacock@urbanelectric.com>
2020-02-24 10:21:38 -0500

    trying the new template file way of life

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -15,18 +15,14 @@ resource aws_ecr_repository "cfb-guide-ecr-repo" {
   name = "cfb-guide-image-repo"
 }
 
-data "template_file" "container_def" {
-  template = "${file("${path.module}/tasks/backend.json")}"
-
-  vars = {
-    image = "${aws_ecr_repository.cfb-guide-ecr-repo.repository_url}"
-    log_group = "${var.cloudwatch_log_group}"
-    log_region = "${var.cloudwatch_log_region}"
-  }
-}
-
 resource "aws_ecs_task_definition" "cfb-guide-graphql-task" {
     family = "cfb-guide-graphql-family"
-    container_definitions = "${data.template_file.container_def.rendered}"
     requires_compatibilities = ["FARGATE"]
+
+    container_definitions = templatefile("${path.module}/tasks/backend.json", {
+      image = "${aws_ecr_repository.cfb-guide-ecr-repo.repository_url}"
+      log_group = "${var.cloudwatch_log_group}"
+      log_region = "${var.cloudwatch_log_region}"
+    })
+    
 }
