111af8f1b4942480e0283570dec553b6b8fac3d5
Kyle Peacock <kyle.peacock@urbanelectric.com>
2020-02-25 14:33:27 -0500

    allow access to GraphQL API

--- main.tf
+++ main.tf
@@ -52,6 +52,13 @@ resource "aws_security_group" "cfb-guide_allow-http-traffic" {
   }
 
   ingress {
+    from_port = 3003
+    to_port = 3003
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol = "tcp"
+  }
+
+  ingress {
     from_port   = 8
     to_port     = 0
     protocol    = "icmp"
@@ -226,6 +233,28 @@ resource "aws_acm_certificate" "cfb-guide-cert" {
 
 resource "aws_cloudwatch_log_group" "cfb-guide-graphql-logs" {
   name = "CFBGuideGraphQL"
+}
+
+resource aws_internet_gateway "cfb-guide_igw" {
+  vpc_id = aws_vpc.cfb-guide-vpc.id
+
+  tags = {
+    Name = "cfb-guide_igw"
+  }
+}
+
+resource aws_route_table "cfb-guide-route-table" {
+  vpc_id = aws_vpc.cfb-guide-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.cfb-guide_igw.id
+  }
+}
+
+resource aws_route_table_association "cfb-guide-route-table-assoc" {
+  subnet_id = aws_subnet.cfb-guide-subnet-one.id
+  route_table_id = aws_route_table.cfb-guide-route-table.id
 }
 
 module "ecs" {
